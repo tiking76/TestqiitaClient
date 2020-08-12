fdea61b97819dad693df17b91a7a98a6b332a437
tiking76 <716yosiki1999@gmail.com>
2020-08-13 02:51:37 +0900

    Initial Commit

---
+++ TestqiitaClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestqiitaClient
+//
+//  Created by 舘佳紀 on 2020/08/13.
+//  Copyright © 2020 Yoshiki Tachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestqiitaClientTests/TestqiitaClientTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestqiitaClientTests.swift
+//  TestqiitaClientTests
+//
+//  Created by 舘佳紀 on 2020/08/13.
+//  Copyright © 2020 Yoshiki Tachi. All rights reserved.
+//
+
+import XCTest
+@testable import TestqiitaClient
+
+class TestqiitaClientTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
